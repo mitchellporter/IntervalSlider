8ca712f9219a5126d0c5443dfc55a45428771190
Mitchell Porter <mitchellporter@gmail.com>
2015-12-24 20:23:06 -0800

    custom slider thumb image

---
+++ Example/Pods/Target Support Files/IntervalSlider/IntervalSlider-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IntervalSlider : NSObject
+@end
+@implementation PodsDummy_IntervalSlider
+@end
---
+++ Example/Pods/Target Support Files/Pods-IntervalSlider_Example/Pods-IntervalSlider_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_IntervalSlider_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_IntervalSlider_ExampleVersionString[];
+
